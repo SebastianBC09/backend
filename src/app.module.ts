76de6e76483e54a8729f9a40613c77adfa915db8
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-10-29 19:29:41 -0500

    feat(app): integrate ConfigModule and MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
+import { validationSchema } from './config/validation.schema';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig],
+      validationSchema,
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+        ...configService.get('database.options'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
